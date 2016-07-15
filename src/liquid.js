39158998ea128c3031003dfeb2f031d8724dd98b
akira.takahashi <rike422@gmail.com>
2016-07-15 13:56:41 +0900

    Extract regular expressions

--- src/liquid.js
+++ src/liquid.js
@@ -1,49 +1,140 @@
+const _FilterSeparator = /\|/;
+const _ArgumentSeparator = /,/;
+const _FilterArgumentSeparator = /\:/;
+const _VariableAttributeSeparator = /\./;
+const _TagStart = /\{\%/;
+const _TagEnd = /\%\}/;
+const _VariableSignature = /\(?[\w\-\.\[\]]\)?/;
+const _VariableSegment = /[\w\-]/;
+const _VariableStart = /\{\{/;
+const _VariableEnd = /\}\}/;
+const _VariableIncompleteEnd = /\}\}?/;
+const _QuotedString = /"[^"]*"|'[^']*'/;
+const _StrictQuotedFragment = /"[^"]+"|'[^']+'|[^\s|:,]+/;
+const _QuotedFragment = RegExp(
+  `${_QuotedString.source}|(?:[^\\s,\\|'\"]|${_QuotedString.source})+`
+);
+
+const _FirstFilterArgument = RegExp(
+  `${_FilterArgumentSeparator.source}(?:${_StrictQuotedFragment.source})`
+);
+
+const _OtherFilterArgument = RegExp(
+  `${_ArgumentSeparator.source}(?:${_StrictQuotedFragment.source})`
+);
+
+const _SpacelessFilter = RegExp(
+  `^(?:'[^']+'|\"[^\"]+\"|[^'\"])*${_FilterSeparator.source}(?:${_StrictQuotedFragment.source})(?:${_FirstFilterArgument.source}(?:${_OtherFilterArgument.source})*)?`
+);
+
+const _Expression = RegExp(
+  `(?:${_QuotedFragment.source}(?:${_SpacelessFilter.source})*)`
+);
+const _TagAttributes = RegExp(`(\\w+)\\s*\\:\\s*(${_QuotedFragment.source})`);
+
+const _AnyStartingTag = /\{\{|\{\%/;
+
+const _PartialTemplateParser = RegExp(
+  `${_TagStart.source}.*?${_TagEnd.source}|${_VariableStart.source}.*?${_VariableIncompleteEnd.source}`
+);
+
+const _TemplateParser = RegExp(`(${_PartialTemplateParser.source}|${_AnyStartingTag.source})`);
+
+const _VariableParser = RegExp(`\\[[^\\]]+\\]|${_VariableSegment.source}+\\??`);
+
 module.exports = class Liquid {
-  static FilterSeparator = /\|/;
-  static ArgumentSeparator = /,/;
-  static FilterArgumentSeparator = /\:/;
-  static VariableAttributeSeparator = /\./;
-  static TagStart = /\{\%/;
-  static TagEnd = /\%\}/;
-  static VariableSignature = /\(?[\w\-\.\[\]]\)?/;
-  static VariableSegment = /[\w\-]/;
-  static VariableStart = /\{\{/;
-  static VariableEnd = /\}\}/;
-  static VariableIncompleteEnd = /\}\}?/;
-  static QuotedString = /"[^"]*"|'[^']*'/;
 
-  static QuotedFragment = RegExp(
-    ((this.QuotedString.source) + "|(?:[^\\s,\\|'\"]|" + (this.QuotedString.source) + ")+")
-  );
+  static get FilterSeparator() {
+    return _FilterSeparator;
+  }
 
-  static StrictQuotedFragment = /"[^"]+"|'[^']+'|[^\s|:,]+/;
+  static get ArgumentSeparator() {
+    return _ArgumentSeparator;
+  }
 
-  static FirstFilterArgument = RegExp(
-    ((this.FilterArgumentSeparator.source) + "(?:" + (this.StrictQuotedFragment.source) + ")")
-  );
+  static get FilterArgumentSeparator() {
+    return _FilterArgumentSeparator;
+  }
 
-  static OtherFilterArgument = RegExp(
-    ((this.ArgumentSeparator.source) + "(?:" + (this.StrictQuotedFragment.source) + ")")
-  );
+  static get VariableAttributeSeparator() {
+    return _VariableAttributeSeparator;
+  }
 
-  static SpacelessFilter = RegExp(
-    ("^(?:'[^']+'|\"[^\"]+\"|[^'\"])*" + (this.FilterSeparator.source) + "(?:" + (this.StrictQuotedFragment.source) + ")(?:" + (this.FirstFilterArgument.source) + "(?:" + (this.OtherFilterArgument.source) + ")*)?")
-  );
+  static get TagStart() {
+    return _TagStart;
+  }
 
-  static Expression = RegExp(
-    ("(?:" + (this.QuotedFragment.source) + "(?:" + (this.SpacelessFilter.source) + ")*)")
-  );
+  static get TagEnd() {
+    return _TagEnd;
+  }
 
-  static TagAttributes = RegExp(("(\\w+)\\s*\\:\\s*(" + (this.QuotedFragment.source) + ")"));
-  static AnyStartingTag = /\{\{|\{\%/;
+  static get VariableSignature() {
+    return _VariableSignature;
+  }
 
-  static PartialTemplateParser = RegExp(
-    ((this.TagStart.source) + ".*?" + (this.TagEnd.source) + "|" + (this.VariableStart.source) + ".*?" + (this.VariableIncompleteEnd.source))
-  );
+  static get VariableSegment() {
+    return _VariableSegment;
+  }
 
-  static TemplateParser = RegExp(
-    ("(" + (this.PartialTemplateParser.source) + "|" + (this.AnyStartingTag.source) + ")")
-  );
+  static get VariableStart() {
+    return _VariableStart;
+  }
 
-  static VariableParser = RegExp(("\\[[^\\]]+\\]|" + (this.VariableSegment.source) + "+\\??"));
+  static get VariableEnd() {
+    return _VariableEnd;
+  }
+
+  static get VariableIncompleteEnd() {
+    return _VariableIncompleteEnd;
+  }
+
+  static get QuotedString() {
+    return _QuotedString;
+  }
+
+  static get QuotedFragment() {
+    return _QuotedFragment;
+  }
+
+  static get StrictQuotedFragment() {
+    return _StrictQuotedFragment;
+  }
+
+  static get FirstFilterArgument() {
+    return _FirstFilterArgument
+  };
+
+  static get OtherFilterArgument() {
+    return _OtherFilterArgument;
+  }
+
+  static get SpacelessFilter() {
+    return _SpacelessFilter;
+  }
+
+  static get Expression() {
+    return _Expression;
+  }
+
+  static get TagAttributes() {
+    return _TagAttributes;
+  }
+
+  static get AnyStartingTag() {
+    return _AnyStartingTag;
+  }
+
+  static get PartialTemplateParser() {
+
+    return _PartialTemplateParser;
+  }
+
+  static get TemplateParser() {
+
+    return _TemplateParser;
+  }
+
+  static VariableParser() {
+    return _VariableParser;
+  }
 };
